3f1fcbef63348512a8c3003498b7b13eec215a1e
etiiizel <franciscoetizel@gmail.com>
2022-10-24 12:32:06 -0300

    changes_II

--- app.js
+++ app.js
@@ -14,3 +14,17 @@ function tapWeiter(element) {
 
 const title = document.querySelector('h1');
 tapWeiter(title);
+const p = document.querySelector('p');
+
+
+function writeP(element) {
+  const textArray = element.innerHTML.split('');
+  element.innerHTML = '';
+  textArray.forEach((letter, index) => {
+    setTimeout( () => {
+      element.innerHTML += letter;
+    }, 75 * index)
+  })
+}
+
+writeP(p);
