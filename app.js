8a343a02ed99abbfb6ce78deaa60ba74773073fa
etiiizel <franciscoetizel@gmail.com>
2022-11-03 15:39:12 -0300

    behavior scroll

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+function tapInit() {
 function tapWriter(element) {
   const textArray = element.innerHTML.split('');
   element.innerHTML = '';
@@ -11,3 +11,26 @@ function tapWriter(element) {
 
 const title = document.querySelector('h1');
 tapWriter(title);
+}
+tapInit();
+
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+
+
+function scrollIntoSection(e) {
+  e.preventDefault();
+  const href = e.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  })
+}
+
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollIntoSection);
+})
